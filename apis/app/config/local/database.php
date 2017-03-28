17f98c52df3e160b03c4469bbf7c521e4f34b091
Suraches Seesat <suraches.see@ascendcorp.com>
2017-03-28 08:43:20 +0700

    update

--- apis/app/config/local/database.php
+++ apis/app/config/local/database.php
@@ -20,17 +20,32 @@ return array(
 
     'connections' => array(
 
+        // 'mysql' => array(
+        //     'driver'    => 'mysql',
+        //     'read' => array(
+        //         'host' => 'localhost', //61.91.1.146
+        //     ),
+        //     'write' => array(
+        //         'host' => 'localhost' // 61.91.1.146
+        //     ),
+        //     'database'  => 'ranbandokmaisod', //authen
+        //     'username'  => 'homestead', // dev
+        //     'password'  => 'secret', // dev!          'password'  => 'secret',
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        // ),
         'mysql' => array(
             'driver'    => 'mysql',
             'read' => array(
-                'host' => 'localhost', //61.91.1.146
+                'host' => '31.220.110.131', //61.91.1.146
             ),
             'write' => array(
-                'host' => 'localhost' // 61.91.1.146
+                'host' => '31.220.110.131' // 61.91.1.146
             ),
-            'database'  => 'ranbandokmaisod', //authen
-            'username'  => 'homestead', // dev
-            'password'  => 'secret', // dev!          'password'  => 'secret',
+            'database'  => 'u877211466_rbdm', //authen
+            'username'  => 'u877211466_rbdm', // dev
+            'password'  => 'Pat@0913763153', // dev!           'password'  => 'secret',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
